acb0795d8dce25fd788e23027ff666caabae9281
Kevin Roshan N <kevinroshann@gmail.com>
2025-02-21 23:55:53 +0530

    fixed alignment

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -127,7 +127,7 @@ const Features = () => (
       </BentoTilt>
 
       <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
-        <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+        <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
           <BentoCard
             src="videos/feature-2.mp4"
             title={
@@ -140,7 +140,7 @@ const Features = () => (
           />
         </BentoTilt>
 
-        <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+        <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
           <BentoCard
             src="videos/feat-3.mp4"
             title={
@@ -166,15 +166,7 @@ const Features = () => (
           />
         </BentoTilt>
 
-        <BentoTilt className="bento-tilt_2">
-          <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
-            <h1 className="bento-title special-font max-w-64 text-black">
-              M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
-            </h1>
-
-            <TiLocationArrow className="m-5 scale-[5] self-end" />
-          </div>
-        </BentoTilt>
+       
 
         <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
           
@@ -188,6 +180,16 @@ const Features = () => (
             description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
             isComingSoon
           />
+        </BentoTilt>
+        
+        <BentoTilt className="bento-tilt_1 me-14 md:col-flex md:me-0">
+          <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+            <h1 className="bento-title special-font max-w-64 text-black">
+              M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
+            </h1>
+
+            <TiLocationArrow className="m-5 scale-[5] self-end" />
+          </div>
         </BentoTilt>
       </div>
     </div>
--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -4,17 +4,10 @@
 // Inside src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./nexus";
+import App from "./app";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// Inside src/App.jsx
 function App() {
   return (
     <div>
