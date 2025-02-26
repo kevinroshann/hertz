3bda24b8b0af6e2abd10886d9b04ddd0d93e2063
John Babu K <johnbabuk@proton.me>
2025-02-26 11:20:33 +0530

    quiz page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Home from "./pages/Home";
 import Events from "./pages/Events";
 import Workshops from "./pages/Workshops";
 import ProjectExpo from "./pages/ProjectExpo";
+import Quiz from "./pages/Quiz";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Route path="/Events" element={<Events />} />
         <Route path="/Workshops" element={<Workshops />} />
         <Route path="/ProjectExpo" element={<ProjectExpo />} />
+        <Route path="/Quiz" element={<Quiz />} /> {/* New Route */}
       </Routes>
     </main>
   );
---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,22 @@
+export default function Quiz() {
+    return (
+      <div
+        className="flex flex-col items-center justify-center min-h-screen w-screen p-4"
+        style={{
+          backgroundImage: "url('/img/world1.webp')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="w-full max-w-5xl h-[80vh] border border-gray-300 shadow-lg rounded-lg overflow-hidden bg-white">
+          <iframe
+            src="https://quiz.konfhub.com/q/hertz-7"
+            title="Hertz-7 Quiz"
+            className="w-full h-full"
+          ></iframe>
+        </div>
+      </div>
+    );
+  }
+  
